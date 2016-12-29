7844cc194630e1eb0a4a53e8b14fdd98bf84c345
buersenlin <buersenlin@126.com>
2016-12-29 15:01:21 +0800

    项目提交

---
+++ src/com/git/Demo.java
@@ -0,0 +1,5 @@
+package com.git;
+
+public class Demo {
+	public String msg = "git demo";
+}
---
+++ src/com/git/main.java
@@ -0,0 +1,11 @@
+package com.git;
+
+public class main {
+
+	public static void main(String[] args) {
+		Demo demo = new Demo();
+		
+		System.out.println(demo.msg);
+	}
+
+}
